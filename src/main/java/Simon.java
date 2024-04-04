7b7e27157367427508d3f93a0430c0180e63368c
AndreaMendez09 <andrea.mendez.sanz01@gmail.com>
2024-04-04 19:42:57 +0200

    refactor: change to char instead of if

--- src/main/java/Simon.java
+++ src/main/java/Simon.java
@@ -1,36 +1,9 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class Simon {
   boolean checkResponse(String simon, String user) {
-    Map<String, String> colorsAndLetters = Map.of(
-      "red", "r",
-      "blue", "b",
-      "green", "g",
-      "yellow", "y"
-    );
-    
-    if (simon.equals("blue")) {
+
+    if (!user.equals(simon.substring(0, 1))) {
       return false;
     }
-
-    if (simon.equals("red")) {
-      return false;
-    }
-
-
-    if (user.equals("g")) {
-      return true;
-    }
-
-    if (simon.equals("green")) {
-      return false;
-    }
-
-    if (simon.equals("yellow red")){
-      return false;
-    }
-
 
     return true;
   }
